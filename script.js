5cb3a1ae90096b8170b43a7051a9696544fcb059
Greg-Diaz <josegregorio1997.lol@gmail.com>
2021-08-11 12:22:12 -0500

    Update script.js
    errores

--- script.js
+++ script.js
@@ -15,80 +15,83 @@ formulario.addEventListener('submit', (e) =>{
 
 
 const cald = (valor, tmon, tcmon) => {
-    
-            if (tmon == 'dolar' ) {
+    let tcambio ='';
+            if (tmon == 'dolar') {
                     if (tcmon == 'pmex') {
-                        tcambio = valor * 19,94 ;
+                        tcambio = valor * 19.94 ;
 
                     } else 
                     if (tcmon == 'pcol'){ 
-                        tcambio = valor * 3947,60 ;
+                        tcambio = valor * 3947.60 ;
                         
                     }else 
                     if (tcmon == 'euro'){
-                        tcambio = valor * 0,85 ;
+                        tcambio = valor * 0.85 ;
                     }else 
-                    if (tcmon == 'lest')
-                        tcambio = valor * 0,72 ;
-
-            } else if(tmon == 'pmex'){
-                    if (tcmon == 'dolar') { 
-                        tcabmio == valor * 0,050;
-                    }else 
-                    if (tcemon == 'pcol'){
-                        tcabmio = valor * 197,94;
-                    }else 
-                    if (tcemon == 'euro'){
-                        tcabmio = valor * 0,043 ;
-                    }else 
-                    if (tcemon == 'lest'){
-                        tcambio = valor * 0,036 ;
+                    if (tcmon == 'lest'){
+                        tcambio = valor * 0.72 ;
                     }
+
+                 } else if (tmon == 'pmex'){
+                    if (tcmon == 'dolar'){
+                        tcambio = valor * 0.050 ;
+                    }else
+                    if (tcmon == 'pcol'){
+                        tcambio = valor * 197.99 ;
+                    }else
+                    if (tcmon == 'euro'){
+                        tcambio = valor * 0.043 ;
+                    }else
+                    if (tcmon == 'lest'){
+                        tcambio = valor * 0.036 ;
+                    }
+                
                 
             }else if (tmon == 'pcol' ){
                     if (tcmon == 'dolar'){ 
-                        tcambio == valor * 0,00025 ;    
+                        tcambio = valor * 0.00025 ;    
                     }else 
                     if (tcmon == 'pmex'){
-                        tcambio == valor * 0,0050 ;
+                        tcambio = valor * 0.0050 ;
                     }else 
                     if (tcmon == 'euro'){
-                        tcambio = valor * 0,00022 ;
+                        tcambio = valor * 0.00022 ;
                     }else 
-                    if (tcemon == 'lest'){
-                        tcambio = valor * 0,00018 ;
+                    if (tcmon == 'lest'){
+                        tcambio = valor * 0.00018 ;
                     }
 
             }else if (tmon == 'euro'){
                     if (tcmon == 'dolar'){
-                        tcambio = valor * 1,17;
+                        tcambio = valor * 1.17 ;
                     } else
                     if (tcmon == 'pmex'){
-                        tcambio = valor * 23,42 ;
+                        tcambio = valor * 23.42 ;
                     }else 
                     if (tcmon == 'pcol'){
-                        tcambio = valor * 4639,05 ;
+                        tcambio = valor * 4639.05 ;
                     }else 
                     if (tcmon == 'lest'){
-                        tcabmio = valor * 0,85 ; 
+                        tcambio = valor * 0.85 ;
                     }
 
             }else if (tmon == 'lest'){
-                    if(tcemon == 'dolar'){
-                        tcambio = valor * 1,39;
+                    if(tcmon == 'dolar'){
+                        tcambio = valor * 1.39 ;
                     }else
-                    if (tcemon == 'pmex'){
-                        tcambio = valor * 27,67 ;
+                    if (tcmon == 'pmex'){
+                        tcambio = valor * 27.67 ;
                     }else 
-                    if (tcemon == 'pcol'){
-                        tcambio = valor * 5481,97 ;
+                    if (tcmon == 'pcol'){
+                        tcambio = valor * 5481.97 ;
 
                     }else
-                    if (tcemon == 'euro') {
-                        tcambio = valor * 1,18 ;
+                    if (tcmon == 'euro') {
+                        tcambio = valor * 1.18 ;
                     }
 
-            } console.log (tcambio) 
-            return tcambio ;
+            } return tcambio ; 
+        
+           
 
 }
